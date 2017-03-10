5ab8cd93c4e5ffe349d1ca23a83502cea864a3fd
Noblesse4Razor <noblesse4razor@gmail.com>
2017-03-10 21:54:57 +0300

    fix

--- binary-search-tree/bst.js
+++ binary-search-tree/bst.js
@@ -7,7 +7,8 @@ function Node(key, data) {
     this._right = null;
     this._parent = null;
 }
-
+Node.prototype.delete=function(key, parent){
+    };
 function insert(current, key, value) {
     if (current === null) return new Node(key, value);
     if (key > current.key) {
@@ -22,65 +23,62 @@ function insert(current, key, value) {
 
 function BinarySearchTree() {
     this._root = null;
-    this.size =0;
+    this.size = 0;
 
-    this.root = function(){
-        return this._root.value;
-    };
-    this.insert = function(key, value){
-        if (key == null || value == null) {
-            throw new Error("argument is error");
-        }
-        this._root = insert(this._root, key, value);
-        this.size++;
-        return this;
-    };
-
-    this.search = function(key){
-        var x = this._root;
-
-        while(x != null){
-            if(x.key == key){
-                return x.value;
-            }
-            if(key < x.key){
-                x = x._left;
-            }
-            else{
-                x = x._right;
-            }
-        }
-    };
-    this.find = function(key){
-        var x = this._root;
-
-        while(x != null){
-            if(x.key == key){
-                return x;
-            }
-            if(key < x.key){
-                x = x._left;
-            }
-            else{
-                x = x._right;
-            }
-        }
-    };
-    
-   this.delete = function (key) {
-       
-   };
-    this.contains= function(value){
-
-    };
-    this.traverse= function(order){
-
-    };
-    this.verify = function () {
-        return true;
-    };
 }
+BinarySearchTree.prototype.root = function(){
+    return this._root.value;
+};
+BinarySearchTree.prototype.insert = function(key, value){
+    if (key == null || value == null) {
+        throw new Error("argument is error");
+    }
+    this._root = insert(this._root, key, value);
+    this.size++;
+    return this;
+};
+BinarySearchTree.prototype.search = function(key){
+    var x = this._root;
 
+    while(x != null){
+        if(x.key == key){
+            return x.value;
+        }
+        if(key < x.key){
+            x = x._left;
+        }
+        else{
+            x = x._right;
+        }
+    }
+};
+BinarySearchTree.prototype.delete = function (key){
+    return this;
+
+};
+BinarySearchTree.prototype.contains= function(val){
+
+    if(this.isEmpty()){
+   
+        return false;
+    }
+  
+    return this._root.contains(val);
+};
+BinarySearchTree.prototype.traverse= function(order){
+
+};
+BinarySearchTree.prototype.verify = function () {
+    return true;
+};
+BinarySearchTree.prototype.isEmpty = function(){
+
+    if(this._root == null){
+        return true;
+    }
+
+    return false;
+};
 // You can comment this block for testing in the browser but final solution MUST contains this module.exports code
 module.exports = {
     BinarySearchTree,

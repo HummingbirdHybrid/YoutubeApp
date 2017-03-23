7c4df982aef5e81a602414a859b3a584162ff8a8
Noblesse4Razor <noblesse4razor@gmail.com>
2017-03-23 22:25:56 +0300

    custom-jquery/feature: implemented 'addCladd', partly append

--- custom-jquery/src/customJQuery.js
+++ custom-jquery/src/customJQuery.js
@@ -2,43 +2,65 @@
     window.$ = function (DOM) {
         if(DOM.nodeName) return new JQ([DOM]);
         return new JQ(document.querySelectorAll(DOM));
-
     };
+    function type(obj) {
+        return Object.prototype.toString.call(obj).slice(8, -1);
+    }
 
     class JQ{
         constructor(DOM){
             this.DOM = DOM;
         }
-        addClass(){
-            return this;
-        }
-        append(){
-            return this;
+
+        addClass(input) {
+          if(type(input) == 'String' && input!='') return this.each(function(){
+                // Split our className property into an array of classes
+                var classes = this.className.split(/\s/);
+                // No need to do anything if the class is already there
+                if (classes.indexOf(input) > -1) return;
+                if (classes.length === 0) this.className = input;
+                this.className += " " + input;
+            });
+          if(type(input) == 'Function')
+              return this.each(function (index) {
+                  $(this).addClass(input.call(this, index,this.getAttribute( "class" )));
+              })
+        };
+
+        append(element) {
+            if (type(element) == "String") return this.each(function () {
+                this.innerHTML += element;
+            });
+            if (element.nodeName) return this.each(function () {
+                let node = document.createElement(element.nodeName);
+                node.innerText = element.innerText;
+                this.appendChild(node);
+            });
         }
         html(){
-            return this;
+            throw new Error('Not implemented')
         }
         attr(){
-            return this;
+            throw new Error('Not implemented')
         }
         children(){
-            return this;
+            throw new Error('Not implemented')
         }
         css(){
-            return this;
+            throw new Error('Not implemented')
         }
         data(){
-            return this;
+            throw new Error('Not implemented')
         }
         on(){
-            return this;
+            throw new Error('Not implemented')
         }
         one(){
-            return this;
+            throw new Error('Not implemented')
         }
         each(callback) {
-            for (var i = 0; i < this.elements.length; i++) {
-                callback.call(this.elements[i], i);
+            for (var i = 0; i < this.DOM.length; i++) {
+                callback.call(this.DOM[i], i);
             }
             return this;
         }
